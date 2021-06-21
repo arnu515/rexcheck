6195a138d49d7bf13f8a00c8acf7a37ff5d32840
arnu515 <arnu515@protonmail.com>
2021-06-21 22:00:49 +0530

    format

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 export { Schema } from "./src/index.ts";
-export { NumberValidator, number } from "./src/validators/number.ts";
-export { StringValidator, string } from "./src/validators/string.ts";
+export { number, NumberValidator } from "./src/validators/number.ts";
+export { string, StringValidator } from "./src/validators/string.ts";
