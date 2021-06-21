737e077aefc2ad23d333de47619ac7a8c187af95
arnu515 <arnu515@protonmail.com>
2021-06-21 21:55:13 +0530

    export functions from the package

---
+++ mod.ts
@@ -0,0 +1,3 @@
+export { Schema } from "./src/index.ts";
+export { NumberValidator, number } from "./src/validators/number.ts";
+export { StringValidator, string } from "./src/validators/string.ts";
