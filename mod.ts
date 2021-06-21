d71a0757a507abc0032d7597c2d9672a3fb604a5
arnu515 <arnu515@protonmail.com>
2021-06-21 22:36:37 +0530

    add metadata fields

--- mod.ts
+++ mod.ts
@@ -1,3 +1,8 @@
 export { Schema } from "./src/index.ts";
 export { number, NumberValidator } from "./src/validators/number.ts";
 export { string, StringValidator } from "./src/validators/string.ts";
+
+export const __VERSION__ = "0.1.2";
+export const __NAME__ = "rexcheck";
+export const __REPO__ = "https://github.com/arnu515/rexcheck";
+export const __AUTHOR__ = { name: "arnu515", email: "arnu515@protonmail.com" };
