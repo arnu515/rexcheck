507f610d3c2409843911456c2dd39f743a9b9b4f
arnu515 <arnu515@protonmail.com>
2021-06-21 22:29:53 +0530

    add doc

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,25 @@ interface Field {
   [key: string]: unknown;
 }
 
+/**
+ * Creates a new Rexcheck schema.
+ * Specify new fields as parameters.
+ *
+ * Examples:
+ * ```typescript
+ * // Create new schema
+ * const schema = new Schema(
+ *   // Create a new string() field
+ *   // Chain methods to validate fields
+ *   string(field_name).required().min(4).alpha(),
+ *
+ *   // Add another field, this time a number() field
+ *   number(field_name).required().integer()
+ * )
+ * ```
+ */
 export class Schema {
+  /** The fields of the schema. Not meant for public use, but we keep it public anyway. */
   fields: Field[];
 
   // deno-lint-ignore no-explicit-any
@@ -16,6 +34,10 @@ export class Schema {
     this.fields = fields;
   }
 
+  /**
+   * Validates the schema.
+   * @param payload An object to validate the schema against
+   */
   validate(payload: Record<string, unknown>): ValidateReturnType {
     for (const field of this.fields) {
       const p = payload[field.field];
